70ef17ee80322dbaf78818cf7cffac67a0294fcd
samuel.park <samuel.park@megazone.com>
2025-05-28 09:10:50 +0900

    chore: environment setting

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:prettier/recommended',
+      'prettier'
+    ],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    rules: {
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single'],
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'error',
+      'no-trailing-spaces': 'error',
+      'no-multi-spaces': 'error',
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-require-imports': 'off',
+      '@typescript-eslint/no-explicit-any': 'off'
+    }
+}; 
